269911244911ee51f8977ce2293f0ef9f219f94f
Derek Burgman <derek@dereekb.com>
2024-02-02 13:07:54 -0600

    dist release

--- src/compat/database/object/snapshot-changes.ts
+++
@@ -1,9 +0,0 @@
-import { Observable, SchedulerLike } from 'rxjs';
-import { DatabaseQuery, SnapshotAction } from '../interfaces';
-import { fromRef } from '../observable/fromRef';
-
-export function createObjectSnapshotChanges<T>(query: DatabaseQuery, scheduler?: SchedulerLike) {
-  return function snapshotChanges(): Observable<SnapshotAction<T>> {
-    return fromRef(query, 'value', 'on', scheduler);
-  };
-}
